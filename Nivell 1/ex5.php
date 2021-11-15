06b9b74cffd81d759cadeb7b645a4c3c2cffbc32
Marc <marc.artco@gmail.com>
2021-11-15 13:09:11 +0100

    ex5

--- Nivell 1/ex5.php
+++ Nivell 1/ex5.php
@@ -1,17 +1,16 @@
 <?php
-$n=33;
 
-if($n>=60){
+function myfunction($n){
+    if($n>=60){ 
     echo "Primera Divisió";
-} else if($n<60 and $n>=45) {
+    } else if($n<60 and $n>=45) {   
     echo "Segona Divisió";
-} else if($n<45 and $n>=33){
+    } else if($n<45 and $n>=33){    
     echo "Tercera Divisió";
-}else if($n<33){
-    echo "L'estudiant reprovarà";
-}
-
-
-
+    }else if($n<33){
+        echo "L'estudiant reprovarà";
+    }
+}   
+echo myfunction(45);//introduint el paràmetre comprovem que funciona.
 
 ?>
