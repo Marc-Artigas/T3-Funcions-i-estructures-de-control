95ef45939bdf87ef1cb9ada594853e705b431418
Marc <marc.artco@gmail.com>
2021-11-15 13:04:57 +0100

    ex5 en curs

---
+++ Nivell 1/ex5.php
@@ -0,0 +1,17 @@
+<?php
+$n=33;
+
+if($n>=60){
+    echo "Primera Divisió";
+} else if($n<60 and $n>=45) {
+    echo "Segona Divisió";
+} else if($n<45 and $n>=33){
+    echo "Tercera Divisió";
+}else if($n<33){
+    echo "L'estudiant reprovarà";
+}
+
+
+
+
+?>
