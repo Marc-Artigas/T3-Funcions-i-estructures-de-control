e5ef904782ed2f48ef984a41d0b6f79a1ec92221
Marc <marc.artco@gmail.com>
2021-11-21 13:37:08 +0100

    ex2 finalitzat

--- Nivell 2/ex2.php
+++ Nivell 2/ex2.php
@@ -1,37 +1,15 @@
 <?php
-
-    $minuts=2;
-    $minutsAddicionals=$minuts-3;
-
+  
+    function costTrucada($minuts){
+        $minutsAddicionals=$minuts-3;
         if($minuts<3){
-            echo "El cost de la trucada és de 10 cèntims";
+            echo "El cost de la trucada és de 10 cèntims".".";
         } else {
             echo "El cost de la trucada és de ".(10+($minutsAddicionals*5).".");
         }
+    }
 
-    // $minuts;
-    // $costTrucada;
-    // $minutsAddicional;
-    // function costTrucada($minuts,$minutsAddicional){
-    //     for($minuts>3;$minuts<=$minutsAddicional;$minuts++){
-    //         echo $minuts."<br>";
-    //         $totalMinuts=$minuts;
-            
-           
-    //     }
-    // }
-
-    // echo costTrucada(3,8);
-    // $costTrucada= 10 + ($minutsAddicional*5);
-    // echo $costTrucada;
-    // if($minuts<3){
-    //     echo "El cost de la trucada és de 10 cèntims";      
-    // } else {
-    //     for($minuts>3;$minuts=$minutsTrucada;$minuts++){
-
-    //     }
-    // }
-
+    echo costTrucada(10);
 
 
 
